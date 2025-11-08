181736ab79b0e3853c85733c0de0998fb1d62d65
r0bay <robbyaw@icloud.com>
2025-11-08 10:36:00 -0700

    Fix AdMob initialization

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -9,7 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Initialize AdMob
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        GADMobileAds.sharedInstance.start(completionHandler: nil)
         // Override point for customization after application launch.
         return true
     }
